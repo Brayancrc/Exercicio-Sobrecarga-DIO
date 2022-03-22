225dd111cfe19d84de228b5eea780743ca9538d6
Brayancrc <brayancrc@gmail.com>
2022-03-22 09:32:14 -0300

    first commit

---
+++ src/brayan/exercicios/dio/Main.java
@@ -0,0 +1,18 @@
+package brayan.exercicios.dio;
+
+public class Main {
+
+    private static QuadriLatero Quadrilatero;
+
+    public static void main(String[] args) {
+
+        // Quadrilátero
+        System.out.println("Exercício quadrilátero");
+        Quadrilatero.area(3);
+        Quadrilatero.area(5d,5d);
+        Quadrilatero.area(7,8,9);
+        Quadrilatero.area(5f,5f);
+
+    }
+
+}
